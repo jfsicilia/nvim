8fab629dc768e14a3f738898d0b0e05e85162884
jfsicilia <jfsicilia@gmail.com>
2026-02-05 13:32:05 +0000

    whick-key config is moved to help.lua plugin

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -17,25 +17,6 @@ return {
     'tpope/vim-rhubarb',
   },
   {
-    -- Hints keybinds
-    'folke/which-key.nvim',
-    opts = {
-      delay = 1000,
-      win = {
-        border = {
-          { '┌', 'FloatBorder' },
-          { '─', 'FloatBorder' },
-          { '┐', 'FloatBorder' },
-          { '│', 'FloatBorder' },
-          { '┘', 'FloatBorder' },
-          { '─', 'FloatBorder' },
-          { '└', 'FloatBorder' },
-          { '│', 'FloatBorder' },
-        },
-      },
-    },
-  },
-  {
     -- Autoclose parentheses, brackets, quotes, etc.
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
